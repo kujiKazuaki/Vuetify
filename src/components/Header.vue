152cae36ad92930ddbd36f609eb2a1aa719a3a26
kujiKazuaki <KujiKazuaki24@gmail.com>
2022-12-24 19:20:05 +0900

    セッティング

---
+++ src/components/Header.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <section class="other_section">
+      <nav><router-link to="/ThingIWantToDo">やりたいこと</router-link>｜</nav>
+    </section>
+  </div>
+</template>
---
+++ src/views/install.vue
@@ -0,0 +1,3 @@
+<template>
+  <h2>インストール方法</h2>
+</template>
